5a03086a18989ba0ba6a1d0aa868068ef9532cd3
PabloFernan <131775404+PabloFernan@users.noreply.github.com>
2025-08-13 14:04:49 -0300

    Criando projeto 001 de js

---
+++ projeto001/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('div#imagem img')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'imagens/manha.jpg'
+        window.document.body.style.background = '#FCD9A7'
+    } else if(hora >= 12 && hora < 18){
+        img.src = 'imagens/tarde.jpg'
+        window.document.body.style.background = '#FF9511'
+    } else {
+        img.src = 'imagens/noite.jpg'
+        window.document.body.style.background = '#0F2226'
+    }
+}
