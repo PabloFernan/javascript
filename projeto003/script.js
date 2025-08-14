7478e24083b78ab4087b72f6eee450236caf5ef5
PabloFernan <131775404+PabloFernan@users.noreply.github.com>
2025-08-14 09:18:50 -0300

    Projeto 3 finalizado-contador

---
+++ projeto003/script.js
@@ -0,0 +1,30 @@
+function contar(){
+    let inicio = window.document.querySelector('input#txtinicio') 
+    let fim = window.document.querySelector('input#txtfim')
+    let passo = window.document.querySelector('input#txtpasso')
+    let resp = window.document.querySelector('div#resp')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        resp.innerHTML = 'Impossível contar!'
+    }else {
+        resp.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p<=0){
+            alert('Passo inválido! Considerando PASSO = 1')
+            p = 1
+        }
+
+        if(i<f){
+            for (let c = i; c<=f ; c+=p){
+                resp.innerHTML += ` ${c} ->`
+            }
+        }else{
+            for (let c = i; c>=f ; c-=p){
+                resp.innerHTML += ` ${c} ->`
+            }
+        }
+         resp.innerHTML += `\u{1F3C1}`
+    }
+}
